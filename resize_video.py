701a53f87035718699eabbaa20d5d606374a8a4b
Piper Keyes <pkeyes12@gmail.com>
2017-09-26 16:45:49 -0700

    resize video actually resizes a video now

--- resize_video.py
+++ resize_video.py
@@ -6,11 +6,7 @@ Created on Thu Sep 21 14:46:53 2017
 """
 import cv2
 import sys
-import math
 import numpy as np
-import pandas as pd
-from utilities import *
-import decimal
 
 
 cap = cv2.VideoCapture("C:\Users\pckeyes\Desktop\\oprm1_morphine.mp4")
@@ -31,12 +27,12 @@ while(cap.isOpened()):
     if not ret:
         print "Frame was not grabbed"
         sys.exit()
-    curr_gamma = np.float32(frame)/255
-    frame = np.uint8(cv2.pow(curr_gamma, .75) * 255)
-    cv2.destroyAllWindows()
+    #curr_gamma = np.float32(frame)/255
+    #frame = np.uint8(cv2.pow(curr_gamma, .75) * 255)
+    #cv2.destroyAllWindows()
     resized = cv2.resize(frame, (640, 360), interpolation = cv2.INTER_AREA)
     #cv2.imshow("resize", resized)
-    cv2.waitKey(5)
+    #cv2.waitKey(5)
     out.write(resized)
 
 cap.release()
