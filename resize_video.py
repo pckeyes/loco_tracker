c36bc299df74625eab364367204349ccfda84914
Piper Keyes <pkeyes12@gmail.com>
2017-09-27 16:41:24 -0700

    took out unneccessary commented out code

--- resize_video.py
+++ resize_video.py
@@ -9,30 +9,21 @@ import sys
 import numpy as np
 
 
-cap = cv2.VideoCapture("C:\Users\pckeyes\Desktop\\oprm1_morphine.mp4")
+cap = cv2.VideoCapture("C:\Users\pckeyes\Desktop\\oprm1_saline.mp4")
 
 if not cap.isOpened():
     print "Video did not open"
     sys.exit()
-    
-#grab first frame to generate resize dimensions
 
 #create video writer object
-out = cv2.VideoWriter("resized_oprm1_morphine.avi", cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 20.0, (640, 360), isColor = True)
-
-
+out = cv2.VideoWriter("resized_oprm1_saline.avi", cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 20.0, (640, 360), isColor = True)
 
 while(cap.isOpened()):
     ret, frame = cap.read()    
     if not ret:
         print "Frame was not grabbed"
-        sys.exit()
-    #curr_gamma = np.float32(frame)/255
-    #frame = np.uint8(cv2.pow(curr_gamma, .75) * 255)
-    #cv2.destroyAllWindows()
+        break
     resized = cv2.resize(frame, (640, 360), interpolation = cv2.INTER_AREA)
-    #cv2.imshow("resize", resized)
-    #cv2.waitKey(5)
     out.write(resized)
 
 cap.release()
